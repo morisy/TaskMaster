e39005b182ac2eefe88832616381dfc9cbe760b7
Michael Morisy <morisy@gmail.com>
2013-09-21 18:09:11 -0400

    It's now printing all the incomplete tasks

--- taskmaster2.py
+++ taskmaster2.py
@@ -16,10 +16,20 @@ for user in userlist:
         print "Compiled user and api key" + str(data)
         print baseurl
         headers = {'content-type': 'application/json', "x-api-user": user, "x-api-key": apikey}
-        
+        #tasks = json.dumps(requests.get(baseurl, headers=headers))
         r = requests.get(baseurl, headers=headers)
         f = open('workfile', 'w')
         f.write(r.text) 
-        print "Response:" + r.text
+       
+
+        
+        tasks = r.json()["tasks"]   
+        keys = tasks.keys()        
+        for key in keys:
+            if tasks[key]["type"] == "todo":
+                if not tasks[key]["completed"]:
+                    print tasks[key]["text"]
+        f = open('tasks', 'w')
+        f.write(tasks)
     except:
         print "Ack!"
