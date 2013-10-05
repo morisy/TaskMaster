0f59df63963a034220ae1cd1157e9bfbd084c4c5
Michael Morisy <morisy@gmail.com>
2013-10-05 12:51:38 -0400

    Pull in users via CSV

--- taskmaster2.py
+++ taskmaster2.py
@@ -5,21 +5,25 @@ import json
 # csvout=csv.writer(csvfile,dialect='excel')
 
 userlist = file('bloglist.txt').readlines()
+
+members_file = 'members.csv'
+members_file = csv.DictReader(open(members_file, 'rb'), delimiter=',', quotechar='"')
+
+#for member in members_file:
+#    print member['Name']
+
+
 baseurl =  "https://habitrpg.com/api/v1/user"
 
-for user in userlist:
-    try:
-        user = user.rstrip()
-        print "Checking user->" + str(user) + "<--"
-        apikey = raw_input("What is this user's API Key?")
-        data = {"x-api-user": user, "x-api-key": apikey}
-        print "Compiled user and api key" + str(data)
-        print baseurl
-        headers = {'content-type': 'application/json', "x-api-user": user, "x-api-key": apikey}
-        
-        r = requests.get(baseurl, headers=headers)
-        f = open('workfile', 'w')
-        f.write(r.text) 
-        print "Response:" + r.text
-    except:
-        print "Ack!"
+for user in members_file:
+    print "Trying " + str(user['Name']) + "."
+    token = user['token']
+    apikey = user['key']
+    data = {"x-api-user": token, "x-api-key": apikey}
+    print "Compiled user and api key" + str(data)
+    print baseurl
+    headers = {'content-type': 'application/json', "x-api-user": token, "x-api-key": apikey}
+    r = requests.get(baseurl, headers=headers)
+    f = open('workfile', 'w')
+    f.write(r.text) 
+    print "Response:" + r.text
